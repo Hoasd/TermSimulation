82d350b01790e9cd1b85ad112852dafa88437651
Horst Ritter <netzritter@gmail.com>
2024-04-20 00:48:11 +0200

    Type gefixt und auf lokales TailwindCSS umgestellt.

--- terminal_input.js
+++ terminal_input.js
@@ -1,11 +1,8 @@
-alert("World");
+const textToShow = '$ curl <span class="text-white">https://app.mailpace.com/api/v1/send</span>\n -H MailPace-Server-Token: a3c4-efg6 \n -d {\n    from: awesome@developer.com,\n    to: important@users.com,\n    subject: Woah, MailPace Rocks!\n} \n\n`<span class="text-gray-500">Sending...</span>`\n^250<span class="text-green-200">✓ Email Sent!</span>';
 
 const typed = new Typed("#terminal-output", {
-  strings: [textToType],
+  strings: [textToShow],
   typeSpeed: 20,
   loop: false,
   cursorChar: "_",
 });
-
-const textToShow =
-  '$ curl <span class="text-white">https://app.mailpace.com/api/v1/send</span>\n -H MailPace-Server-Token: a3c4-efg6 \n -d {\n    from: awesome@developer.com,\n    to: important@users.com,\n    subject: Woah, MailPace Rocks!\n} \n\n`<span class="text-gray-500">Sending...</span>`\n^250<span class="text-green-200">✓ Email Sent!</span>';
